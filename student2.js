e26c664474584016189e8de2fc79674b029ddd43
Chris <cpaterson152@gmail.com>
2023-06-15 16:32:24 -0700

    Lab 10 specs

--- student2.js
+++ student2.js
@@ -3,7 +3,11 @@ var companyname4 = "Echoes of Innovation dvds";
 var address4 = "269 Main St. Valencia, CA 91344";
 var phonenumber5 = "(555) 861-1234";
 var imgurl797 = 'http://www.college1.com/images/';
+var cardimgurl247 = 'http://www.college1.com/images/cards/gbCard';
 
+var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer"};
+var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer"};
+var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer"};
 
 const headerContainer = document.getElementById('head269');
 const footerContainer = document.getElementById('foot861');
@@ -78,7 +82,13 @@ product3Btn.addEventListener('click', function() {
 
 
 //Lab Assignment #10
-var cardimgurl247 = 'http://www.college1.com/images/cards/gbCard';
+
+
+var imgurl797 = 'http://www.college1.com/images/';   // global
+// . . . 
+var produrl = imgurl797 + myproduct.id + '.gif';  // local
+// creates string containing 'http://www.college1.com/images/4426.gif'
+// insert produrl into the display of product1
 
 var cardurl = cardimgurl247 + cardnumber + '.gif';
 // where cardnumber is 0 to 51 for a face up card and 52 for the back
