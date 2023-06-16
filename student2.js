c64d044dfe9440636986ba0b062bccae411793a3
Chris <cpaterson152@gmail.com>
2023-06-15 17:02:19 -0700

    Lab 11 specs

--- student2.js
+++ student2.js
@@ -5,7 +5,7 @@ var phonenumber5 = "(555) 861-1234";
 var imgurl797 = 'http://www.college1.com/images/';
 var cardimgurl247 = 'http://www.college1.com/images/cards/gbCard';
 
-var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer"};
+var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer", img:'assets/boombox.jpg'};
 var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer"};
 var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer"};
 
@@ -45,20 +45,23 @@ getFooter(companyname4, address4, phonenumber5);
 // Lab Assignment #8 & 9
 function makeMenu4(size) {
   menuContainer.innerHTML += `
-
   `;
 }
 
 
 
 function makeMain2(myproduct) {
+  var produrl = imgurl797 + myproduct.id + '.gif';  // local
   mainContainer.innerHTML = `
     <h2>My Product: </h2><br>
     <h3>Product Name: ${myproduct.name}</h3><br>
     <h5>Product ID: ${myproduct.id}</h5><br>
     <h5>Product Description: ${myproduct.desc}</h5><br>
     <h5>Shopping Cart Link Bar </h5><br>
-    <h5>Product Image: ${imgurl797}${myproduct}</h5><br>
+    <h5>Product Image:<br>
+      <img src="${myproduct.img}" width="500">
+      <img src="${produrl}" width="500">
+    </h5><br>
   `;
 }
 
@@ -82,13 +85,15 @@ product3Btn.addEventListener('click', function() {
 
 
 //Lab Assignment #10
-var produrl = imgurl797 + myproduct.id + '.gif';  // local
-// creates string containing 'http://www.college1.com/images/4426.gif'
-// insert produrl into the display of product1
-
 var cardurl = cardimgurl247 + cardnumber + '.gif';
 // where cardnumber is 0 to 51 for a face up card and 52 for the back
 // insert cardurl into an img tag to display cards, DO NOT forget id='cardX'
+
+var cardnum779 = -1;
+
+function hitCard870() {
+  
+}
 
 function dealCards412() {
 
