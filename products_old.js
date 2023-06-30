103848d1ef157d099ec2c3d78ffdff90693d8bf9
Chris <cpaterson152@gmail.com>
2023-06-29 20:37:59 -0700

    Comment out unused products

--- products_old.js
+++ products_old.js
@@ -1,3 +1,4 @@
-var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer"};
-var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer"};
-var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer"};
+// var product1 = { name:"HP 4426", id:"4426", desc:"Newest and Best Laptop from HP Computer"};
+// var product2 = { name:"Apple 88123 iPad", id:"88123", desc:"Apple Tablet Computer"};
+// var product3 = { name:"Dell Dimension 2400", id:"2400X", desc:"A fast 2.4 ghz computer"};
+
